7c7b800717565f8f3354b11d9988d4e006e08911
jdquevedo09 <jdquevedo09@gmail.com>
2020-12-10 14:40:15 -0500

    Update App

--- src/database/dbConfig.js
+++ src/database/dbConfig.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const URI = 'mongodb://127.0.0.1:27017/tasks';
+const URI = process.env.MONGO_URI;
 
 
-mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
-db.once('open',_=>console.log('database connected in', URI));
+db.once('open', _ => console.log('database connected in', URI));
 
-db.on('error',error=>console.log(error));
+db.on('error', error => console.log(error));
